7d438dbfe1380d0282e9beea84826f08e84a84be
Fabrizio Coscia <fabricoscia@gmail.com>
2022-12-04 17:24:40 -0300

    Add users endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
