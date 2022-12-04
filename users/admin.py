1eff444dd43279b7be8520dc7d1c28761c4ea730
Fabrizio Coscia <fabricoscia@gmail.com>
2022-12-04 18:05:48 -0300

    Add admin models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import UserProfile
+
+from django.contrib.auth.admin import UserAdmin
+
+
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+
+
+UserAdmin.inlines = [ProfileInline]
